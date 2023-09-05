78182a8b23bd9dbde3f995cbb8d958fe1c761d37
KASHISHRAI2003 <144108341+KASHISHRAI2003@users.noreply.github.com>
2023-09-05 15:00:47 +0530

    cmt_msg_022

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # lab04_assignment
-hello
+
 ## a new line added inside VS code
--- python_assignment.py
+++ python_assignment.py
@@ -1,46 +1,59 @@
 class Employee:
-    def _init_(self, employee_id, name, age, salary):
-        self.employee_id = employee_id
+    def __init__(self, emp_id, name, age, salary):
+        self.emp_id = emp_id
         self.name = name
         self.age = age
         self.salary = salary
 
 class EmployeeDatabase:
-    def _init_(self):
+    def __init__(self):
         self.employees = []
 
-    def add_employee(self, employee):
-        self.employees.append(employee)
+    def add_employee(self, emp):
+        self.employees.append(emp)
 
-    def search(self, key, value, operator):
-        results = []
-        for employee in self.employees:
-            if operator == '<' and getattr(employee, key) < value:
-                results.append(employee)
-            elif operator == '>' and getattr(employee, key) > value:
-                results.append(employee)
-            elif operator == '<=' and getattr(employee, key) <= value:
-                results.append(employee)
-            elif operator == '>=' and getattr(employee, key) >= value:
-                results.append(employee)
-        return results
+    def search_by_age(self, operator, age):
+        result = []
+        for emp in self.employees:
+            if operator == '>' and emp.age > age:
+                result.append(emp)
+            elif operator == '<' and emp.age < age:
+                result.append(emp)
+            elif operator == '>=' and emp.age >= age:
+                result.append(emp)
+            elif operator == '<=' and emp.age <= age:
+                result.append(emp)
+        return result
 
-    def display_employees(self, employees):
-        if not employees:
-            print("No matching records found.")
-        else:
-            print("Employee ID\tName\tAge\tSalary")
-            for employee in employees:
-                print(f"{employee.employee_id}\t{employee.name}\t{employee.age}\t{employee.salary}")
+    def search_by_salary(self, operator, salary):
+        result = []
+        for emp in self.employees:
+            if operator == '>' and emp.salary > salary:
+                result.append(emp)
+            elif operator == '<' and emp.salary < salary:
+                result.append(emp)
+            elif operator == '>=' and emp.salary >= salary:
+                result.append(emp)
+            elif operator == '<=' and emp.salary <= salary:
+                result.append(emp)
+        return result
+
+    def search_by_employee_id(self, emp_id):
+        result = []
+        for emp in self.employees:
+            if emp.emp_id == emp_id:
+                result.append(emp)
+        return result
 
 def main():
-    db = EmployeeDatabase()
+    database = EmployeeDatabase()
 
-    db.add_employee(Employee("161E90", "Raman", 41, 56000))
-    db.add_employee(Employee("161F91", "Himadri", 38, 67500))
-    db.add_employee(Employee("161F99", "Jaya", 51, 82100))
-    db.add_employee(Employee("171E20", "Tejas", 30, 55000))
-    db.add_employee(Employee("171G30", "Ajay", 45, 44000))
+    
+    database.add_employee(Employee("161E90", "Raman", 41, 56000))
+    database.add_employee(Employee("161F91", "Himadri", 38, 67500))
+    database.add_employee(Employee("161F99", "Jaya", 51, 82100))
+    database.add_employee(Employee("171E20", "Tejas", 30, 55000))
+    database.add_employee(Employee("171G30", "Ajay", 45, 44000))
 
     while True:
         print("\nMenu:")
@@ -51,26 +64,37 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            key = 'age'
-            operator = input("Enter operator (<, >, <=, >=): ")
-            value = int(input("Enter age: "))
+            operator = input("Enter operator (>, <, <=, >=): ")
+            age = int(input("Enter age: "))
+            results = database.search_by_age(operator, age)
+            print("\nSearch results:")
+            for emp in results:
+                print(f"Employee ID: {emp.emp_id}, Name: {emp.name}, Age: {emp.age}, Salary: {emp.salary}")
+
         elif choice == '2':
-            key = 'salary'
-            operator = input("Enter operator (<, >, <=, >=): ")
-            value = float(input("Enter salary: "))
+            operator = input("Enter operator (>, <, <=, >=): ")
+            salary = float(input("Enter salary: "))
+            results = database.search_by_salary(operator, salary)
+            print("\nSearch results:")
+            for emp in results:
+                print(f"Employee ID: {emp.emp_id}, Name: {emp.name}, Age: {emp.age}, Salary: {emp.salary}")
+
         elif choice == '3':
-            key = 'employee_id'
-            value = input("Enter Employee ID: ")
-            operator = '=='
+            emp_id = input("Enter Employee ID: ")
+            results = database.search_by_employee_id(emp_id)
+            if results:
+                emp = results[0]
+                print("\nSearch result:")
+                print(f"Employee ID: {emp.emp_id}, Name: {emp.name}, Age: {emp.age}, Salary: {emp.salary}")
+            else:
+                print("Employee not found.")
+
         elif choice == '4':
             print("Exiting program.")
             break
+
         else:
-            print("Invalid choice. Please select a valid option.")
-            continue
+            print("Invalid choice. Please try again.")
 
-        results = db.search(key, value, operator)
-        db.display_employees(results)
-
-if __name__ == "_main_":
+if __name__ == "__main__":
     main()
